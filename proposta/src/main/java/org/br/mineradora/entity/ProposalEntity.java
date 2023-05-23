92e73bcf38f95b0d2aae4cc2cb2debb1437f940d
luisagpurificacao <luisa.purificacao16@gmail.com>
2023-05-23 15:18:26 -0300

    proposal entity added

---
+++ proposta/src/main/java/org/br/mineradora/entity/ProposalEntity.java
@@ -0,0 +1,26 @@
+package org.br.mineradora.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "proposal")
+@Data
+@NoArgsConstructor
+public class ProposalEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String customer;
+    @Column(name = "price_tonne")
+    private BigDecimal priceTonne;
+    private Integer tonnes;
+    private String country;
+    @Column(name = "proposal_validity_days")
+    private Integer proposalValidityDays;
+
+}
