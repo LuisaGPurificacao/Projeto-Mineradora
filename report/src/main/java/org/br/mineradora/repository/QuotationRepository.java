38cf26f5b241092a34932f8a8eb39bdf73c7974f
luisagpurificacao <luisa.purificacao16@gmail.com>
2023-05-24 11:10:32 -0300

    adding repositories

--- report/src/main/java/org/br/mineradora/repository/QuotationRepository.java
+++ report/src/main/java/org/br/mineradora/repository/QuotationRepository.java
@@ -1,10 +1,10 @@
 package org.br.mineradora.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import org.br.mineradora.entity.OpportunityEntity;
+import org.br.mineradora.entity.QuotationEntity;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class OpportunityRepository implements PanacheRepository<OpportunityEntity> {
+public class QuotationRepository implements PanacheRepository<QuotationEntity> {
 }
